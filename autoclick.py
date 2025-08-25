395b47e6c1aa07253d3a75b2fb99d17c8cd313f2
Maateuss77 <programadormateusa@gmail.com>
2025-08-25 00:40:05 -0300

    version alpha 1

--- autoclick.py
+++ autoclick.py
@@ -1,23 +1,34 @@
 import pyautogui
 import time
+import keyboard
 
-def autoclick(interval=1.0):
+def autoclick(interval=1.0, playPause="="):
     print("Autoclick iniciado")
-    try:
-        while True:
+    running = False  
+
+    while True:
+        if keyboard.is_pressed("esc"):
+            print("Programa encerrado")
+            break
+
+        if keyboard.is_pressed(playPause):
+            running = not running
+            print("Autoclick ON" if running else "Autoclick OFF")
+            time.sleep(0.3)
+
+        if running:
             pyautogui.click()
             time.sleep(interval)
-    except KeyboardInterrupt:
-        print("Autoclick finalizado")
 
 def main():
-    print("Autoclicker Simples")
+    print("==========Autoclicker Simples==========")
     try:
         interval = float(input("Digite o intervalo entre cliques (em segundos, ex: 0.5): "))
+        playPause = input("Digite a tecla para iniciar/parar o autoclick (padrão '='): ") or "="
     except ValueError:
         print("Valor inválido. Usando intervalo padrão de 1 segundo.")
         interval = 1.0
     input("Pressione Enter para iniciar o autoclick. Pressione Ctrl+C para parar.")
-    autoclick(interval)
+    autoclick(interval = interval, playPause=playPause)
 
 main()
